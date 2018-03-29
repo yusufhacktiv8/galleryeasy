0d33bb78e00125f81a08b4eda607f2e05fe65e5c
myyusuf <myyusuf1911@gmail.com>
2018-03-30 03:28:03 +0700

    Fix styling

--- src/components/TagImage.jsx
+++ src/components/TagImage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TAG_IMAGE_URL = '/images/tagimage.png';
 
 export default ({ transparent, onClick }) => {
-  const className = transparent ? 'tag-img transparent-70' : 'tag-img';
+  const className = transparent ? 'tag-img tag-img-transparent' : 'tag-img';
   return (
     <img src={TAG_IMAGE_URL} className={className} onClick={onClick} alt="Tag" />
   );
