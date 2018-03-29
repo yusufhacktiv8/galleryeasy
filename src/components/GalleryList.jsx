e0c5859fb4740ef1773db04ee096a5c3d6d1c6e8
myyusuf <myyusuf1911@gmail.com>
2018-03-29 23:19:42 +0700

    Add onItemFavouriteClicked props to GalleryList

--- src/components/GalleryList.jsx
+++ src/components/GalleryList.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import GalleryListItem from './GalleryListItem';
 
-export default ({ items }) => {
+export default ({ items, onItemFavouriteClicked }) => {
   return (
     <div className="container">
       <div className="row">
         {
           items.map(item => (
             <div className="col-3" key={item.id}>
-              <GalleryListItem url={item.url} favourited={item.favourited} />
+              <GalleryListItem
+                url={item.url}
+                favourited={item.favourited}
+                onFavouriteClicked={favourited => onItemFavouriteClicked(item.id, favourited)}
+              />
             </div>
           ))
         }
--- src/components/GalleryList.test.js
+++ src/components/GalleryList.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
 import GalleryList from './GalleryList';
+import GalleryListItem from './GalleryListItem';
+import TagImage from './TagImage';
 
 test('GalleryList correctly rendered', () => {
   const items = [
@@ -20,4 +24,25 @@ test('GalleryList correctly rendered', () => {
   );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test('GalleryList trigger onItemFavouriteClicked when GalleryListItem TagImage clicked', () => {
+  const items = [
+    {
+      id: 1,
+      url: '/test1.png',
+      favourited: true,
+    },
+    {
+      id: 2,
+      url: '/test2.png',
+      favourited: false,
+    },
+  ];
+  const callback = sinon.spy();
+  const favourited = true;
+  const element = mount(<GalleryList items={items} onItemFavouriteClicked={callback} />);
+  
+  element.find(GalleryListItem).first().find(TagImage).simulate('click');
+  expect(callback.calledWith(1, favourited)).toBe(true);
 });
