9b5a8883cf2287ef9267fa8659883e768addc83a
myyusuf <myyusuf1911@gmail.com>
2018-03-30 18:37:38 +0700

    Add fetch more feature

--- src/components/GalleryListContainer.jsx
+++ src/components/GalleryListContainer.jsx
@@ -13,28 +13,40 @@ const IMAGES_API_URL = 'http://api.giphy.com/v1/gifs/search';
 const imageTypes = { original: 'original', fixedHeight: 'fixed_height', fixedHeightStill: 'fixed_height_still' };
 export default class GalleryListContainer extends Component {
   state = {
+    search: '',
+    start: 0,
     images: [],
     favourites: [],
     loading: false,
   }
 
   onSearch = (search) => {
+    this.setState({
+      search,
+      start: 0,
+    });
     if (search && search.length > 0) {
-      this.fetchImages(search);
+      this.setState({
+        loading: true,
+      });
+      this.fetchImages(search, 0)
+        .then((response) => {
+          const images = response.data.data.map(obj => (
+            {
+              id: obj.id,
+              url: obj.images[imageTypes.fixedHeightStill].url,
+              favourited: this.state.favourites.indexOf(obj.id) !== -1,
+            }
+          ));
+          this.setState({
+            images,
+          });
+        });
     }
   }
 
-  fetchImages(search = '') {
-    this.setState({
-      loading: true,
-    });
-    axios.get(IMAGES_API_URL, {
-      params: {
-        q: search,
-        api_key: process.env.REACT_APP_API_KEY,
-        limit: IMAGES_LIMIT,
-      },
-    })
+  fetchMore = () => {
+    this.fetchImages(this.state.search, this.state.start + IMAGES_LIMIT)
       .then((response) => {
         const images = response.data.data.map(obj => (
           {
@@ -44,16 +56,37 @@ export default class GalleryListContainer extends Component {
           }
         ));
         this.setState({
-          loading: false,
-          images,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-        this.setState({
-          loading: false,
+          images: [...this.state.images, ...images],
+          start: this.state.start + IMAGES_LIMIT,
         });
       });
+  }
+
+  fetchImages(search = '', offset = 0) {
+    return new Promise((resolve, reject) => {
+      axios.get(IMAGES_API_URL, {
+        params: {
+          q: search,
+          api_key: process.env.REACT_APP_API_KEY,
+          limit: IMAGES_LIMIT,
+          offset,
+        },
+      })
+        .then((response) => {
+          this.setState({
+            loading: false,
+          }, () => {
+            resolve(response);
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          this.setState({
+            loading: false,
+          });
+          reject(error);
+        });
+    });
   }
 
   toggleFavourite = (id, favourited) => {
@@ -97,6 +130,18 @@ export default class GalleryListContainer extends Component {
               items={images}
               onItemFavouriteClicked={this.toggleFavourite}
             />)
+          }
+        </div>
+        <div className="fetch-more-button-container">
+          {
+            (this.state.images.length >= IMAGES_LIMIT) ? (
+              <input
+                className="fetch-more-button"
+                type="button"
+                value="Fetch More"
+                onClick={this.fetchMore}
+              />
+            ) : null
           }
         </div>
         <div className="footer-container">
